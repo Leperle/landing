e6f6e94d7be6ad5c094cd7b1ecace9642e896427
Clint.Network <me@clint.network>
2022-01-27 14:53:51 +0200

    Add 3 layers illustration

--- components/Hero.vue
+++ components/Hero.vue
@@ -57,46 +57,7 @@
 				</div>
 			</div>
 			<div class="mt-20 sm:mt-24 lg:mt-0 lg:col-span-6 hidden lg:flex justify-end">
-				<svg xmlns="http://www.w3.org/2000/svg" class="w-full p-20" style="position: absolute; width: 580px; top: 110px;" viewBox="0 0 545.936 628.168">
-					<defs>
-						<linearGradient id="linear-gradient" x1="1.204" y1="-0.074" x2="0.306" y2="1.04" gradientUnits="objectBoundingBox">
-							<stop offset="0" stop-color="#7939ff"/>
-							<stop offset="1" stop-color="#b064fe"/>
-						</linearGradient>
-					</defs>
-					<g id="Group_42" data-name="Group 42" transform="translate(-477.032 -435.916)">
-						<g id="Group_37" data-name="Group 37">
-							<path id="Path_16" data-name="Path 16" d="M837.3,661.2V591.957L656.885,487.781,484.476,587.094a14.827,14.827,0,0,0-7.444,12.84v60.7L656.885,557.024Z" fill="url(#linear-gradient)"/>
-							<path id="Path_17" data-name="Path 17" d="M869.892,786.964l59.961-35.586V537.214L757.443,437.9a14.823,14.823,0,0,0-14.827,0l-52.518,30.6L869.922,572.088Z" fill="url(#linear-gradient)"/>
-						</g>
-						<path id="Path_18" data-name="Path 18" d="M664.684,591.245,484.446,694.888a14.823,14.823,0,0,0-7.414,12.809v356.387h59.961V733.971L726.958,624.9Z" fill="url(#linear-gradient)"/>
-						<path id="Path_19" data-name="Path 19" d="M630.108,994.128V717.069L570.147,752v311.963h59.961Z" fill="url(#linear-gradient)"/>
-						<g id="Group_39" data-name="Group 39">
-							<g id="Group_38" data-name="Group 38">
-								<path id="Path_20" data-name="Path 20" d="M837.3,661.2V591.957L656.885,487.781,484.476,587.094a14.827,14.827,0,0,0-7.444,12.84v60.7L656.885,557.024Z" fill="url(#linear-gradient)"/>
-							</g>
-							<path id="Path_21" data-name="Path 21" d="M664.684,591.245,484.446,694.888a14.823,14.823,0,0,0-7.414,12.809v356.387h59.961V733.971L726.958,624.9Z" fill="url(#linear-gradient)"/>
-							<path id="Path_22" data-name="Path 22" d="M630.108,994.128V717.069L570.147,752v311.963h59.961Z" fill="url(#linear-gradient)"/>
-						</g>
-						<g id="Group_40" data-name="Group 40">
-							<path id="Path_23" data-name="Path 23" d="M842.551,902.615,1022.968,798.44V599.756a14.827,14.827,0,0,0-7.355-12.811l-52.577-30.632V763.537L782.62,867.712Z" fill="url(#linear-gradient)"/>
-							<path id="Path_24" data-name="Path 24" d="M656.885,833.491l.594,69.243,185.665,107.112,172.41-99.342a14.827,14.827,0,0,0,7.414-12.84V836.99L843.144,941.166Z" fill="url(#linear-gradient)"/>
-							<path id="Path_25" data-name="Path 25" d="M656.885,941.166v69.243l85.761,49.315a14.83,14.83,0,0,0,14.827,0l52.577-30.1Z" fill="url(#linear-gradient)"/>
-							<path id="Path_26" data-name="Path 26" d="M963.036,556.43V764.219L782.62,868.394l59.931,34.34,180.417-104.175V599.875a14.827,14.827,0,0,0-7.355-12.812Z" fill="url(#linear-gradient)"/>
-							<path id="Path_27" data-name="Path 27" d="M656.885,833.491l.594,69.243,185.665,107.675,172.44-99.876a14.828,14.828,0,0,0,7.384-12.841v-60.7L843.144,941.166Z" fill="url(#linear-gradient)"/>
-							<path id="Path_28" data-name="Path 28" d="M656.885,1010.409l85.761,49.315a14.83,14.83,0,0,0,14.827,0l52.577-30.1L656.974,941.166Z" fill="url(#linear-gradient)"/>
-						</g>
-						<g id="Group_41" data-name="Group 41">
-							<path id="Path_29" data-name="Path 29" d="M842.551,902.615,1022.968,798.44V599.756a14.827,14.827,0,0,0-7.355-12.811l-52.577-30.632V763.537L782.62,867.712Z" fill="url(#linear-gradient)"/>
-							<path id="Path_30" data-name="Path 30" d="M656.885,833.491l.594,69.243,185.665,107.112,172.41-99.342a14.827,14.827,0,0,0,7.414-12.84V836.99L843.144,941.166Z" fill="url(#linear-gradient)"/>
-							<path id="Path_31" data-name="Path 31" d="M656.885,941.166v69.243l85.761,49.315a14.83,14.83,0,0,0,14.827,0l52.577-30.1Z" fill="url(#linear-gradient)"/>
-							<path id="Path_32" data-name="Path 32" d="M963.036,556.43V764.219L782.62,868.394l59.931,34.34,180.417-104.175V599.875a14.827,14.827,0,0,0-7.355-12.812Z" fill="url(#linear-gradient)"/>
-							<path id="Path_33" data-name="Path 33" d="M656.885,833.491l.594,69.243,185.665,107.675,172.44-99.876a14.828,14.828,0,0,0,7.384-12.841v-60.7L843.144,941.166Z" fill="url(#linear-gradient)"/>
-							<path id="Path_34" data-name="Path 34" d="M656.885,1010.409l85.761,49.315a14.83,14.83,0,0,0,14.827,0l52.577-30.1L656.974,941.166Z" fill="url(#linear-gradient)"/>
-						</g>
-					</g>
-				</svg>
-				<!--				<img class="bouncing" alt="bouncing-logo" style="width: 70%;" src="https://raw.githubusercontent.com/parasol-finance/white-paper/main/logo.png" />-->
+				<MainIllustration />
 			</div>
 		</div>
 	</section>
---
+++ components/MainIllustration.vue
@@ -0,0 +1,99 @@
+<template>
+	<div class="w-full relative">
+		<img class="absolute w-full bounce1" alt="l" src="~/assets/images/Logo.png" />
+		<img class="absolute w-full bounce2 spin" alt="l" src="~/assets/images/Logo_P.png" />
+		<img class="absolute w-full bounce3 spin2" alt="l" src="~/assets/images/Logo_Q.png" />
+ 	</div>
+</template>
+
+<script>
+export default {
+	name: "MainIllustration"
+}
+</script>
+
+<style scoped>
+div
+{
+	margin-top: -30px;
+}
+
+.bounce1
+{
+	animation: float 15s ease-in-out infinite;
+}
+
+.bounce2
+{
+	animation: float2 5s ease-in-out infinite;
+}
+
+.bounce3
+{
+	animation: float3 10s ease-in-out infinite;
+}
+
+.spin
+{
+	animation: spin 421s linear infinite;
+}
+
+.spin2
+{
+	animation: spin 421s linear infinite;
+	animation-delay: 2s;
+}
+
+@keyframes float {
+	0% {
+		transform: translatey(0px);
+	}
+
+	50% {
+		transform: translatey(-1rem);
+	}
+
+	100% {
+		transform: translatey(0px);
+	}
+}
+
+@keyframes float2 {
+	0% {
+		transform: translatey(-10px);
+	}
+
+	50% {
+		transform: translatey(0);
+	}
+
+	100% {
+		transform: translatey(-10px);
+	}
+}
+
+@keyframes float3 {
+	0% {
+		transform: translatey(10px);
+	}
+
+	50% {
+		transform: translatey(0);
+	}
+
+	100% {
+		transform: translatey(10px);
+	}
+}
+
+@keyframes spin {
+	0% {
+		transform: rotate(0);
+	}
+
+	100% {
+		transform: rotate(360deg);
+	}
+}
+
+</style>
